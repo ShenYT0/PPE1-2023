950d9e0c72c2dac3e27c283280ab221621476362
ShenYT0 <shenyuntian0@gmail.com>
2023-11-13 12:16:34 +0100

    mettre à jour miniprojet

--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -14,8 +14,11 @@ fi
 var=1
 while read -r line;
 do
-	code=$(curl -s -I $line | head -n 1)
-	encodage=$(curl -s -I $line | grep "content-type:")
+	code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line) #$(curl -s -I $line | head -n 1)
+	encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -P -o "charset=\S+" | cut -d "=" -f2) #$(curl -s -I $line | grep "content-type:")
 	echo -e "$var\t$line\t$code\t$encodage\t"
 	var=$(expr $var + 1)
 done < $1
+
+# curl -s -I -L -w "%{http_code}" -o /dev/null $URL
+# curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d "=" -f2
